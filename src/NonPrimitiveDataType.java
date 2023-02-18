5282e7805728c887ccc6c81e274a77b64c911d33
Morrisonjr <morrisonjr.g@gmail.com>
2023-02-17 23:03:14 -0500

    Test commit

---
+++ src/NonPrimitiveDataType.java
@@ -0,0 +1,17 @@
+public class NonPrimitiveDataType {
+
+
+    public static void main(String [] args){
+        String name = "Greg";
+        System.out.println("Name: " + name );
+
+        System.out.println("Length of name: " + name.length());
+
+
+
+
+
+
+    }
+
+}
---
+++ src/week1/DataTypes.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class DataTypes {
+    public static void main(String[]args){
+
+
+    }
+
+
+
+}
